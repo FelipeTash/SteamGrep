4aa672e31fb1704198bf7897fe6b667ed63becd1
Felipe da Mata <felipenv01.764@gmail.com>
2025-09-20 13:52:36 -0300

    Update client.c

--- client.c
+++ client.c
@@ -1,16 +1,24 @@
-// Este é o arquivo principal.
+// Este é o arquivo de teste.
 
 #include <stdio.h>
-#include <windows.h>
 #include "steam_emu.h"
 
 int main(void)
 {
     // Verifica se a SteamAPI_init é presente
     if (!SteamAPI_Init()) {
-        printf("Erro: não foi possível inicializar a Steam API!");
+        printf("Erro: não foi possível inicializar a SteamAPI.");
         return -1;
+    }
+    // Inicializa como InitSafe
+    if (!SteamAPI_InitSafe()) {
+        printf("Erro: não foi possível inicializar como InitSafe");
+        return -1;
+    } else {
+        printf("inicializado como InitSafe");
     }
     
     printf("SteamApi inicializada.");
+
+
 }
