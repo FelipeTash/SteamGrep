b1ab9a9782fd97a04e723ab570b47fd4c9246364
Felipe da Mata <felipenv01.764@gmail.com>
2025-09-20 13:48:35 -0300

    Update steam_api.c

--- steam_api.c
+++ steam_api.c
@@ -3,3 +3,10 @@
 bool SteamAPI_Init(void) {
     return true; // stub: sempre inicializa
 }
+bool SteamAPI_InitSafe(void) {
+    return true;
+    return "Safe Init";
+}
+
+// gcc -shared -o steam_api.dll steam_api.c "-Wl,--out-implib,libsteam_api.a"
+// gcc -shared -o steam_api64.dll steam_api.c "-Wl,--out-implib,libsteam_api.a"
